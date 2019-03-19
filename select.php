b08b6808b3b2ba0f48be163234e8a332d183d871
Zakaria Moslim <z.zza@hotmail.com>
2019-03-19 21:43:43 +0100

    reading data,,, added new file (select.php)

---
+++ select.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+      $db = mysqli_connect('localhost', 'root', '', 'php');
+      $sql = 'SELECT * FROM users';
+      $result = mysquli_query($db, $sql);
+
+      foreach ($result as $row) {
+        printf('<li><span style="color: %s">%s (%s)</span></li>',
+          htmlspecialchars($row['color']),
+          htmlspecialchars($row['name']),
+          htmlspecialchars($row['gender'])
+        );
+      }
+      mysqli_close($db);
+     ?>
+  </body>
+</html>
